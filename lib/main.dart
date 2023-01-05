f0554dfc16544c43e23e0ed8f9876bc3c5a43927
Julie <jem.baret@gmail.com>
2023-01-05 14:15:09 +0100

    extract the card with a module OneSpot

--- lib/main.dart
+++ lib/main.dart
@@ -15,18 +15,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Map spot = Map.unmodifiable({
-      'Surf Break': 'Reef Break',
-      'Photos':
-          'https://dl.airtable.com/ZuXJZ2NnTF40kCdBfTld_thomas-ashlock-64485-unsplash.jpg',
-      'Address': 'Pipeline, Oahu, Hawaii'
-    });
-
-    // String spotName = 'Reef Break';
-    // String spotURL =
-    //     'https://dl.airtable.com/ZuXJZ2NnTF40kCdBfTld_thomas-ashlock-64485-unsplash.jpg';
-    // String spotLocation = 'Pipeline, Oahu, Hawaii';
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Surfify',
@@ -56,54 +44,73 @@ class MyApp extends StatelessWidget {
           ),
           backgroundColor: Color.fromARGB(255, 59, 130, 166),
         ),
-        body: SafeArea(
-          child: Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("images/background.jpg"),
-                  fit: BoxFit.cover),
-            ),
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Container(
-                  // margin: const EdgeInsets.all(40.0),
-                  height: 400,
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10),
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color.fromARGB(255, 185, 184, 184),
-                        blurRadius: 6.0,
-                        spreadRadius: 2.0,
-                        offset: Offset(0.0, 3),
-                      )
-                    ],
-                  ),
-                  child: Column(
-                    children: [
-                      Text(
-                        spot['Surf Break'],
-                        textScaleFactor: 3,
-                        // style: const TextStyle(fontSize: 18),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Image(
-                          image: NetworkImage(spot['Photos']),
-                        ),
-                      ),
-                      Text(
-                        spot['Address'],
-                        textScaleFactor: 2,
-                        // style: const TextStyle(fontSize: 18),
-                      ),
-                    ],
-                  ),
+        body: OneSpot(),
+      ),
+    );
+  }
+}
+
+class OneSpot extends StatelessWidget {
+  const OneSpot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Map spot = Map.unmodifiable({
+      'Surf Break': 'Reef Break',
+      'Photos':
+          'https://dl.airtable.com/ZuXJZ2NnTF40kCdBfTld_thomas-ashlock-64485-unsplash.jpg',
+      'Address': 'Pipeline, Oahu, Hawaii'
+    });
+
+    // String spotName = 'Reef Break';
+    // String spotURL =
+    //     'https://dl.airtable.com/ZuXJZ2NnTF40kCdBfTld_thomas-ashlock-64485-unsplash.jpg';
+    // String spotLocation = 'Pipeline, Oahu, Hawaii';
+    return SafeArea(
+      child: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("images/background.jpg"), fit: BoxFit.cover),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              // margin: const EdgeInsets.all(40.0),
+              height: 400,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
                 ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromARGB(255, 185, 184, 184),
+                    blurRadius: 6.0,
+                    spreadRadius: 2.0,
+                    offset: Offset(0.0, 3),
+                  )
+                ],
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    spot['Surf Break'],
+                    textScaleFactor: 3,
+                    // style: const TextStyle(fontSize: 18),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image(
+                      image: NetworkImage(spot['Photos']),
+                    ),
+                  ),
+                  Text(
+                    spot['Address'],
+                    textScaleFactor: 2,
+                    // style: const TextStyle(fontSize: 18),
+                  ),
+                ],
               ),
             ),
           ),
