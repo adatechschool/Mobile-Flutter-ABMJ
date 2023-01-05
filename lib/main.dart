90fe9b084de907e9b3c8b837842eadd5eecdb120
Julie <jem.baret@gmail.com>
2023-01-05 17:37:57 +0100

    add: routes and buttons to switch pages

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,6 @@ class MyApp extends StatelessWidget {
         ),
       ),
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 200, 231, 233),
         appBar: AppBar(
           title: Center(
             child: Column(children: [
@@ -66,53 +65,129 @@ class OneSpot extends StatelessWidget {
     // String spotURL =
     //     'https://dl.airtable.com/ZuXJZ2NnTF40kCdBfTld_thomas-ashlock-64485-unsplash.jpg';
     // String spotLocation = 'Pipeline, Oahu, Hawaii';
-    return SafeArea(
-      child: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("images/background.jpg"), fit: BoxFit.cover),
-        ),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Container(
-              // margin: const EdgeInsets.all(40.0),
-              height: 400,
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Color.fromARGB(255, 185, 184, 184),
-                    blurRadius: 6.0,
-                    spreadRadius: 2.0,
-                    offset: Offset(0.0, 3),
-                  )
-                ],
-              ),
-              child: Column(
-                children: [
-                  Text(
-                    spot['Surf Break'],
-                    textScaleFactor: 3,
-                    // style: const TextStyle(fontSize: 18),
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("images/background.jpg"), fit: BoxFit.cover),
+      ),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                // margin: const EdgeInsets.all(40.0),
+                height: 400,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image(
-                      image: NetworkImage(spot['Photos']),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromARGB(255, 185, 184, 184),
+                      blurRadius: 6.0,
+                      spreadRadius: 2.0,
+                      offset: Offset(0.0, 3),
+                    )
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      spot['Surf Break'],
+                      // textScaleFactor: 3,
+                      style: const TextStyle(fontSize: 40),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image(
+                        image: NetworkImage(spot['Photos']),
+                      ),
+                    ),
+                    Text(
+                      spot['Address'],
+                      style: GoogleFonts.lato(
+                        textStyle: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontStyle: FontStyle.italic,
+                            fontSize: 20),
+                        // style: const TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: ElevatedButton(
+                  child: Text(
+                    'All Spots',
+                    style: GoogleFonts.lato(
+                      textStyle: TextStyle(fontSize: 15),
                     ),
                   ),
-                  Text(
-                    spot['Address'],
-                    textScaleFactor: 2,
-                    // style: const TextStyle(fontSize: 18),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const SpotsLists()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(255, 59, 130, 166),
                   ),
-                ],
+                ),
               ),
-            ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SpotsLists extends StatelessWidget {
+  const SpotsLists({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Surfify',
+      theme: ThemeData(
+        textTheme: GoogleFonts.sacramentoTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Column(children: [
+              Text(
+                '🏄‍♀️ Surfify',
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
+              ),
+              Text(
+                'Locate the best spot on Earth !',
+                style: GoogleFonts.lato(
+                  textStyle:
+                      TextStyle(fontStyle: FontStyle.italic, fontSize: 10),
+                ),
+              )
+            ]),
+          ),
+          backgroundColor: Color.fromARGB(255, 59, 130, 166),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Go back!'),
           ),
         ),
       ),
