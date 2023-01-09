e188292a988aea44edd244d84850c117580a89b3
Julie <jem.baret@gmail.com>
2023-01-09 15:38:50 +0100

    passing args throw modal

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ void main() {
   runApp(const MyApp());
 }
 
+//This class define the SurfSpot objects for later
 class SurfSpot {
   String name;
   String url;
@@ -21,6 +22,7 @@ class SurfSpot {
   }
 }
 
+//Our main widget : contain appbar, theme and body
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -34,6 +36,10 @@ class MyApp extends StatelessWidget {
           Theme.of(context).textTheme,
         ),
       ),
+      routes: {
+        ExtractArgumentsScreen.routeName: (context) =>
+            const ExtractArgumentsScreen(),
+      },
       home: Scaffold(
         appBar: AppBar(
           title: Center(
@@ -60,6 +66,103 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// You can pass any object to the arguments parameter.
+// In this example, create a class that contains both
+// a customizable title and message.
+class ScreenArguments {
+  final String name;
+  final String url;
+  final String address;
+
+  ScreenArguments(this.name, this.url, this.address);
+}
+
+// A Widget that extracts the necessary arguments from
+// the ModalRoute.
+class ExtractArgumentsScreen extends StatelessWidget {
+  const ExtractArgumentsScreen({super.key});
+
+  static const routeName = '/extractArguments';
+
+  @override
+  Widget build(BuildContext context) {
+    // Extract the arguments from the current ModalRoute
+    // settings and cast them as ScreenArguments.
+    final args = ModalRoute.of(context)!.settings.arguments as ScreenArguments;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          args.name,
+          style: GoogleFonts.lato(),
+        ),
+        backgroundColor: Color.fromARGB(255, 59, 130, 166),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("images/background.jpg"), fit: BoxFit.cover),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  // margin: const EdgeInsets.all(40.0),
+                  height: 400,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromARGB(255, 185, 184, 184),
+                        blurRadius: 6.0,
+                        spreadRadius: 2.0,
+                        offset: Offset(0.0, 3),
+                      )
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      Text(
+                        args.name,
+                        // textScaleFactor: 3,
+                        style: const TextStyle(fontSize: 40),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image(
+                          image: NetworkImage(args.url),
+                        ),
+                      ),
+                      Text(
+                        args.address,
+                        style: GoogleFonts.lato(
+                          textStyle: TextStyle(
+                              fontWeight: FontWeight.w300,
+                              fontStyle: FontStyle.italic,
+                              fontSize: 20),
+                          // style: const TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//List our spots
 class MyList extends StatelessWidget {
   const MyList({super.key});
 
@@ -85,32 +188,57 @@ class MyList extends StatelessWidget {
         'https://dl.airtable.com/e3QoP3cFSyykZJOvWGIy_cesar-couto-477018-unsplash%20(1).jpg',
         'Supertubes, Jeffreys Bay, South Africa'));
 
-    return ListView.separated(
-      padding: const EdgeInsets.all(8),
-      itemCount: spots.length,
-      itemBuilder: (BuildContext context, int index) {
-        return Container(
-          height: 50,
-          child: Center(
-            child: Column(
-              children: [
-                Text('${spots[index].name}', style: GoogleFonts.lato()
-                    // textScaleFactor: 3,
-                    ),
-                Text(
-                  '${spots[index].address}',
-                  style: GoogleFonts.lato(),
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("images/background.jpg"), fit: BoxFit.cover),
+      ),
+      child: ListView.separated(
+        padding: const EdgeInsets.all(10),
+        itemCount: spots.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 60,
+            child: Center(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Color.fromARGB(255, 59, 130, 166),
                 ),
-              ],
+                onPressed: () {
+                  // When the user taps the button,
+                  // navigate to a named route and
+                  // provide the arguments as an optional
+                  // parameter.
+                  Navigator.pushNamed(
+                    context,
+                    ExtractArgumentsScreen.routeName,
+                    arguments: ScreenArguments('${spots[index].name}',
+                        '${spots[index].url}', '${spots[index].address}'),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Text(
+                      '${spots[index].name}',
+                      style: GoogleFonts.lato(fontSize: 25),
+                    ),
+                    Text(
+                      '${spots[index].address}',
+                      style: GoogleFonts.lato(),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
-        );
-      },
-      separatorBuilder: (BuildContext context, int index) => const Divider(),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+      ),
     );
   }
 }
 
+//Details about one spot + background image
 class OneSpot extends StatelessWidget {
   const OneSpot({super.key});
 
@@ -211,6 +339,7 @@ class OneSpot extends StatelessWidget {
   }
 }
 
+//Test widget
 class SpotsLists extends StatelessWidget {
   const SpotsLists({super.key});
 
