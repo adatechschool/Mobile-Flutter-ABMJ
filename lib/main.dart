0ccdb3ef61b9ca0b7747cdb26ad72b197baaa1e1
Julie <jem.baret@gmail.com>
2023-01-09 12:27:11 +0100

    new try with listview homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+//trying with listview
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
